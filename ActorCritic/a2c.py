a8ad95aa9d996893d948fccd17c6ce605fd5c35a
zyr17 <jzjqz17@gmail.com>
2020-04-16 14:54:48 +0800

    update

--- ActorCritic/a2c.py
+++ ActorCritic/a2c.py
@@ -13,7 +13,7 @@ from baselines.common.atari_wrappers import wrap_deepmind, make_atari
 TXSW = tensorboardX.SummaryWriter
 
 from common.envs import TransposeImage, make_vec_envs
-from common.models import AtariCNN
+from common.models import AtariCNN, ActorCriticNet
 from common.utils import cuda, Fake_TXSW
 from common.storage import RolloutStorage
 from common.distributions import FixedCategorical
@@ -25,24 +25,6 @@ try:
 except:
     def profile(func):
         return func
-
-#input: state; output: policy distribution, value function
-class A2CNet(torch.nn.Module):
-    def __init__(self, inputlen, featurelen, action_space, feature_net):
-        super(A2CNet, self).__init__()
-        self.feature_net = feature_net(inputlen, featurelen)
-        self.p_fc = torch.nn.Linear(featurelen, action_space)
-        self.softmax = torch.nn.Softmax(dim = 1)
-        self.v_fc = torch.nn.Linear(featurelen, 1)
-
-    def forward(self, inputs, apply_softmax = False):
-        x = inputs
-        x = self.feature_net(x)
-        v = self.v_fc(x)
-        pg = self.p_fc(x)
-        if apply_softmax:
-            pg = self.softmax(pg)
-        return pg, v
 
 class ReplayBuffer:
     def __init__(self, maxlen):
@@ -77,7 +59,7 @@ class A2C:
         self.GAMMA = gamma
         self.MAX_FRAMES = max_frames
         self.FRAME = 0
-        self.model = cuda(A2CNet(inputlen, hiddenlen, envs.action_space.n, AtariCNN))
+        self.model = cuda(ActorCriticNet(inputlen, hiddenlen, envs.action_space.n, AtariCNN))
         self.update_count = 0
         self.LR = learning_rate
         self.BATCH_SIZE = batch_size
@@ -132,40 +114,31 @@ class A2C:
         #print(next_v + reward.unsqueeze(1), v)
         #pdb.set_trace()
         v_loss = torch.nn.functional.mse_loss(reward, v)
-
+        '''
         if not hasattr(self, 'LAST_OUTPUT') or self.FRAME - 10000 > self.LAST_OUTPUT:
             print(pd_loss.item(), self.ENTROPY_BETA * entropy_loss.item(), v_loss.item(), v.mean().item())
             self.LAST_OUTPUT = self.FRAME
+        '''
+        self.TXSW.add_scalar('pd_loss', pd_loss.item(), self.FRAME)
+        self.TXSW.add_scalar('entropy_loss', entropy_loss.item(), self.FRAME)
+        self.TXSW.add_scalar('v_loss', v_loss.item(), self.FRAME)
+        self.TXSW.add_scalar('v_mean', v.mean().item(), self.FRAME)
         loss = pd_loss + self.ENTROPY_BETA * entropy_loss + v_loss
         loss.backward()
         torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.CLIP_GRAD)
         self.opt.step()
         return loss.item()
-
-    def update_policy(self, state, action, reward, next_state, ist):
-        self.buffer.append([state, action, reward, next_state, ist])
-        if len(self.buffer) == self.BATCH_SIZE:
-            state_b, action_b, reward_b, next_state_b, ist_b = zip(*self.buffer)
-            state_b = torch.tensor(np.stack(state_b)).float()
-            action_b = torch.tensor(np.stack(action_b))
-            reward_b = torch.tensor(np.stack(reward_b))
-            next_state_b = torch.tensor(np.stack(next_state_b)).float()
-            ist_b = torch.tensor(np.stack(ist_b))
-            self.buffer.clear()
-            return self.real_update_policy(state_b, action_b, reward_b, next_state_b, ist_b)
-        return None
     
     @profile
-    def sampling(self):
+    def main(self):
         start_time = time.time()
         state = self.env.reset()
         init_state = state
         self.rollouts.obs[0].copy_(torch.tensor(init_state))
-        step = 0
         self.rollouts.step = 0
         tot_reward = torch.zeros(self.threads, 1).float()
         last_loss = 0
-        blabla = False
+        find_terminate = False
         while True:
             self.FRAME += 1
             if self.render > 0 and self.FRAME % self.render == 0:
@@ -179,7 +152,7 @@ class A2C:
                                  np.zeros((self.threads, 1)), reward, np.expand_dims(1 - ist, 1), np.zeros((self.threads, 1)))
 
             if self.rollouts.step == self.MAX_STEP:
-                if blabla:
+                if find_terminate:
                     pass#pdb.set_trace()
                 with torch.no_grad():
                     _, next_v = self.model(self.rollouts.obs[-1])
@@ -188,11 +161,14 @@ class A2C:
                 self.rollouts.after_update()
                 self.TXSW.add_scalar('loss', loss, self.FRAME - self.MAX_STEP)
                 last_loss = loss
-                blabla = False
+                find_terminate = False
 
             for num, i in enumerate(ist):
                 if i:
-                    blabla = True
+                    # if multilive, don't push to self.PREVIOUS_REWARD. TODO: record every live seperately
+                    if 'ale.lives' in _[num].keys() and _[num]['ale.lives'] > 0:
+                        continue
+                    find_terminate = True
                     self.PREVIOUS_REWARD.append(tot_reward[num].item())
                     self.TXSW.add_scalar('reward', tot_reward[num], len(self.PREVIOUS_REWARD))
                     tot_reward[num] = 0
@@ -200,7 +176,12 @@ class A2C:
                         return
                 
             if self.FRAME % self.LOG_INTERVAL == 0 and len(self.PREVIOUS_REWARD) > 0:
-                print('Frame %7d, epoch %6d, %5d steps, %.1f steps/s, loss %4.4f, %4.2f' % (self.FRAME, len(self.PREVIOUS_REWARD), self.FRAME * self.threads, self.FRAME * self.threads / (time.time() - start_time), last_loss, self.PREVIOUS_REWARD[-1]))
+                print('Frame %7d, epoch %6d, %5d steps, %.1f steps/s, loss %4.4f, %4.2f'
+                      % (self.FRAME, len(self.PREVIOUS_REWARD), self.FRAME * self.threads,
+                         self.FRAME * self.threads / (time.time() - start_time), last_loss,
+                         sum(self.PREVIOUS_REWARD[-self.EVAL_LENGTH:]) / self.EVAL_LENGTH
+                        )
+                     )
 
             state = next_s
 
@@ -218,19 +199,14 @@ class A2C:
                     self.TXSW.close()
                     return True
         return False
-
-    def main(self):
-        self.sampling()
             
-
 def a2c_main():
     #Pong CNN
     threads = 32
-    env = TransposeImage(wrap_deepmind(make_atari('PongNoFrameskip-v4')))
-    envs = make_vec_envs('PongNoFrameskip-v4', 0, threads, None, None, False)
+    envs = make_vec_envs('BreakoutNoFrameskip-v4', 0, threads, None, None, False)
     a2c = A2C(envs, gamma = 0.99, learning_rate = 0.0001, max_frames = 10000000, threads = threads, 
-            batch_size = 64, max_step = 5, render = -1, target_reward = 18,
-            TXComment='A2C_Pong')
+            batch_size = 64, max_step = 5, render = -1, target_reward = 300,
+            TXComment='')
     a2c.main()
 
 if __name__ == "__main__":
--- common/models.py
+++ common/models.py
@@ -19,3 +19,20 @@ class AtariCNN(nn.Module):
         x = self.fc(x)
         return x
 
+#input: state; output: policy distribution, value function
+class ActorCriticNet(torch.nn.Module):
+    def __init__(self, inputlen, featurelen, action_space, feature_net):
+        super(ActorCriticNet, self).__init__()
+        self.feature_net = feature_net(inputlen, featurelen)
+        self.p_fc = torch.nn.Linear(featurelen, action_space)
+        self.softmax = torch.nn.Softmax(dim = 1)
+        self.v_fc = torch.nn.Linear(featurelen, 1)
+
+    def forward(self, inputs, apply_softmax = False):
+        x = inputs
+        x = self.feature_net(x)
+        v = self.v_fc(x)
+        pg = self.p_fc(x)
+        if apply_softmax:
+            pg = self.softmax(pg)
+        return pg, v
